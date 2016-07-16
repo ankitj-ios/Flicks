de9d35c65cc4790104a7baab5cb4d9d34a8e8c9a
Ankit Jasuja <AJasuja@walmartlabs.com>
2016-07-16 15:53:14 -0700

    code formatting and documentation

--- Flicks/FlicksHomeViewController.swift
+++ Flicks/FlicksHomeViewController.swift
@@ -20,6 +20,7 @@ class FlicksHomeViewController: UIViewController, UITableViewDelegate, UITableVi
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         /*reachibility code starts*/
         do {
             let reachability = try Reachability.reachabilityForInternetConnection()
@@ -32,17 +33,24 @@ class FlicksHomeViewController: UIViewController, UITableViewDelegate, UITableVi
         } catch {
             print(error)
         }
-        /*reachibility code ends*/
-        
+
+        /* setting delegate and datasource for tableview */
         flicksHomeTableView.delegate = self
         flicksHomeTableView.dataSource = self
         
+        /* pull to refresh */
+        let refreshControl =  UIRefreshControl()
+        refreshControl.addTarget(<#T##target: AnyObject?##AnyObject?#>, action: <#T##Selector#>, forControlEvents: <#T##UIControlEvents#>)
+        
+        
+        /* calling api to fetch data */
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
         let request = NSURLRequest(URL: url!)
-        
+
+        /* show progress heads up display*/
         MBProgressHUD.showHUDAddedTo(self.view, animated: true)
-        
+
         let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
                                    delegate: nil, delegateQueue: NSOperationQueue.mainQueue())
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
@@ -51,16 +59,15 @@ class FlicksHomeViewController: UIViewController, UITableViewDelegate, UITableVi
             if let data = dataOrNil {
                 if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(data,
                     options:[]) as? NSDictionary {
-                    //                    NSLog("response: \(responseDictionary)")
+                    // cache output from api
                     self.movies = responseDictionary["results"] as! [NSDictionary]
+                    // reload table view
                     self.flicksHomeTableView.reloadData()
                 }
                 
             }
         });
         task.resume()
-
-
     }
 
     override func didReceiveMemoryWarning() {
@@ -70,17 +77,17 @@ class FlicksHomeViewController: UIViewController, UITableViewDelegate, UITableVi
     
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        /*total cell count*/
+        /* total cell count */
         return movies.count
     }
     
     /* function called for every cell*/
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        //current index cell data
+        /* current index cell data */
         let movie = movies[indexPath.row] as NSDictionary
-        //get current cell
+        /* get current cell */
         let movieCell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
-        //set current cell
+        /* set current cell */
         movieCell.movieTitle.text = movie["title"] as? String
         movieCell.movieDescription.text = movie["overview"] as? String
         let baseUrl = "http://image.tmdb.org/t/p"
@@ -91,13 +98,11 @@ class FlicksHomeViewController: UIViewController, UITableViewDelegate, UITableVi
         return movieCell
     }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        /* get source and destination */
         let sourceCell = sender as! MovieCell
         let destinationViewController = segue.destinationViewController as! FlicksDetailsViewController
-        
+        /*pass data from source to destination */
         let indexPath = flicksHomeTableView.indexPathForCell(sourceCell)
         destinationViewController.movie = movies[indexPath!.row]
         
